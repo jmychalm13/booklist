37df3b8385e00701184dd0641d9da19617dda0aa
“Jessica <jmychalm13@gmail.com>
2023-12-07 18:50:56 -0600

    seeded db and added gems and installed gems

--- server.rb
+++ server.rb
@@ -1,4 +1,24 @@
 require "sinatra"
+require "mongoid"
+
+# DB Setup
+Mongoid.load! "mongoid.config"
+
+# Models
+class Book
+  include Mongoid::Document
+
+  field :title, type: String
+  field :author, type: String
+  field :isbn, type: String
+
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :isbn, presence: true
+
+  index({ title: "text" })
+  index({ isbn: 1 }, { unique: true, name: "isbn_index" })
+end
 
 get "/" do
   "Welcome to BookList!"
